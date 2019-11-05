7d7d2bd5ca0ca2b0bba354aa782451258c3e27b4
luthomasly <luthomasly@gmail.com>
2019-11-05 21:38:59 +0100

    réappretissage Java

---
+++ CourRecre/src/CourRecre.java
@@ -0,0 +1,22 @@
+
+public class CourRecre {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int prem = 17;
+		int deux = 7;
+		int trois = 5;
+		int quatre = 2;
+		
+		int surface = 0;
+		int perimetre = 0;
+		int cote = 0;
+		
+		cote = (5*prem) + (2*deux) + trois + (2*quatre);
+		//System.out.println(cote);
+		
+		System.out.println(surface = (cote * cote));
+		System.out.println(perimetre = cote + cote + cote + cote);
+	}
+
+}
